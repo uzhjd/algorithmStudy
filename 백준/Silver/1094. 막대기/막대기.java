83fab3f6ba02fcbd582a7dbaebe0ba944e3c66be
uzhjd <73466440+uzhjd@users.noreply.github.com>
2023-01-26 23:06:01 +0900

    fix: 코드 정렬

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-int answer = 0;
-int original = 64;
-int x = scanner.nextInt();
-int count = 1;
-while (true) {
-if (original > x - answer) {
-int half = original / 2;
-if (original - half >= x - answer) {
-original = original - half;
-} else {
-count++;
-answer = answer + half;
-original = half;
-}
-} else {
-break;
-}
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int answer = 0;
+        int original = 64;
+        int count = 1;
 
-System.out.println(count);
-}
+        int x = scanner.nextInt();
+
+        while (true) {
+            if (original > x - answer) {
+                int half = original / 2;
+                if (original - half >= x - answer) {
+                    original = original - half;
+                } else {
+                    count++;
+                    answer = answer + half;
+                    original = half;
+                }
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
