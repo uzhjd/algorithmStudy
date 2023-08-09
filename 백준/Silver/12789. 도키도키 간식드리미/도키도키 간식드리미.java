2e65481dbe290b0ebf3be19de665614e9ba5cb85
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-08-09 18:12:01 +0900

    Update 도키도키 간식드리미.java

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,50 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		Queue<Integer> q = new LinkedList<>();
-		Stack<Integer> s = new Stack<>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < n; i++) {
-			q.offer(Integer.parseInt(st.nextToken()));
-		}
-		
-		int start = 1;
-		
-		while(!q.isEmpty()) {
-			if(q.peek() == start) {
-				q.poll();
-				start++;
-			}else if(!s.isEmpty() && s.peek() == start) {
-				s.pop();
-				start++;
-			}else {
-				s.push(q.poll());
-			}
-		}
-        
-		while(!s.isEmpty()) {
-			if(s.peek() == start) {
-				s.pop();
-				start++;
-			}else {
-				System.out.println("Sad");
-				return;
-			}
-		}
-        
-		System.out.println("Nice");
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int order = 1;
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            queue.add(sc.nextInt());
+        }
+
+        int i = 0;
+
+        while(!queue.isEmpty()) {
+            if(order == queue.peek()) {
+                queue.poll();
+                order++;
+            } else if(!stack.isEmpty() && order == stack.peek()) {
+                stack.pop();
+                order++;
+            } else {
+                stack.add(queue.poll());
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            if(order == stack.peek()) {
+                stack.pop();
+                order++;
+            } else {
+                System.out.println("Sad");
+                return;
+            }
+        }
+
+        System.out.println("Nice");
+
+        return;
+    }
 }
