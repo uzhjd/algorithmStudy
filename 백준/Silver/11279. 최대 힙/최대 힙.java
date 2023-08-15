397f3d388c7db0a17e6400d8585afb8eceac2741
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-08-15 15:02:38 +0900

    Update 최대 힙.java

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -5,19 +5,26 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
+    public static void main(String[] args)  throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
 
-            if (num == 0)
-                sb.append(que.size() == 0 ? 0 : que.poll()).append('\n');
-            else que.add(num);
+        for(int i = 0; i < N; i++) {
+            int  X = Integer.parseInt(bf.readLine());
+
+            if(X == 0) {
+                sb.append(pq.size() == 0 ? 0 : pq.poll()).append('\n');
+            } else {
+                pq.add(X);
+            }
         }
+
         System.out.println(sb.toString());
+
+        return;
     }
 }
