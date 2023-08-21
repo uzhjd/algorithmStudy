116f7c356658f607a891695ad17edff181a9dd97
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-08-21 15:23:50 +0900

    Update 단어 뒤집기.java

--- Inflearn/문자열/4. 단어 뒤집기/단어 뒤집기.java
+++ Inflearn/문자열/4. 단어 뒤집기/단어 뒤집기.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-  
+
 public class Main {
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    
-    int n = Integer.parseInt(sc.nextLine());
-    
-    for(int i = 0; i < n; i++) {
-      String str = sc.nextLine();
-      StringBuffer sb = new StringBuffer(str);
-      
-      str = sb.reverse().toString();
-      
-      System.out.println(str);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        for(int i = 0; i < n; i++) {
+            StringBuffer sb = new StringBuffer(sc.nextLine());
+            
+            System.out.println(sb.reverse().toString());
+        }
+
+        return ;
     }
-    
-    return ;
-  }
 }
