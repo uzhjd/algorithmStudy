5495fc770fbe19c9529abbd66ca2b247bddb6ad0
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-08-31 17:34:50 +0900

    [level 2] Title: 가장 큰 수, Time: 1.88 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,21 +4,23 @@ class Solution {
     public String solution(int[] numbers) {
         String answer = "";
         String[] arr = new String[numbers.length];
-
-        for(int i = 0; i < numbers.length; i++){
-            arr[i] = Integer.toString(numbers[i]);
-        }
         
-        Arrays.sort(arr, new Comparator<String>() {
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, new Comparator<>() {
             @Override
-            public int compare(String a, String b) {
-                return (b+a).compareTo(a+b);
+            public int compare(String s1, String s2) {
+                return (s2 + s1).compareTo(s1 + s2);
             }
         });
         
         if (arr[0].equals("0")) return "0";
         
-        for(String s: arr) answer += s;
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
         
         return answer;
     }
