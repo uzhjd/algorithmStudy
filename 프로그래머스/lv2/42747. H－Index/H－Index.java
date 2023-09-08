3d32cf91dc8a126c841c19f45e00c9e1b1d53d51
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-09-08 16:05:18 +0900

    [level 2] Title: H-Index, Time: 0.35 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
+            
+            if(smaller >= answer)
+                answer = Math.max(answer, smaller);
+            else
+                break;
+        }
+        return answer;
+    }
+}
