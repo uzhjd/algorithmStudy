c4fee62bf02af95e763ca96e497ec996f12d53e0
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-12-15 02:02:40 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.96 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -8,33 +8,30 @@ class Solution {
         return BFS(x,y,n);
     }
     
-    int BFS(int x, int y, int n){
-        int cnt = -1;
+    int BFS(int x, int y, int n) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x, 0});
         
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             int[] now = q.poll();
             
-            if(now[0] == y){
-                cnt = now[1];
-                break;
-            }
+            if(now[0] == y) return now[1];
             
-            if(now[0] + n <= y && !visited[now[0] + n]){
+            if(now[0] + n <= y && !visited[now[0] + n]) {
                 q.add(new int[]{now[0] + n, now[1] + 1});
                 visited[now[0] + n] = true;
-            }
-            if(now[0] * 2 <= y && !visited[now[0] * 2]){
+            } 
+            if(now[0] * 2 <= y && !visited[now[0] * 2]) {
                 q.add(new int[]{now[0] * 2, now[1] + 1});
                 visited[now[0] * 2] = true;
             }
-            if(now[0] * 3 <= y && !visited[now[0] * 3]){
+            if(now[0] * 3 <= y && !visited[now[0] * 3]) {
                 q.add(new int[]{now[0] * 3, now[1] + 1});
                 visited[now[0] * 3] = true;
             }
         }
         
-        return cnt;
+        return -1;
+        
     }
 }
