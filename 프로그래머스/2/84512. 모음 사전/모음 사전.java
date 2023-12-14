2b31c4121d5d15e2d8cb6f8e04c2cc11e3f9c22a
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-12-15 02:23:40 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 class Solution {
+    
     public int solution(String word) {
-        String vowels = "AEIOU";
-        int[] change = {781, 156, 31, 6, 1};
         int answer = word.length();
+        String vowels = "AEIOU";
+        int[] change = new int[]{781, 156, 31, 6, 1};
         
         for(int i = 0; i < word.length(); i++) {
             answer += change[i] * vowels.indexOf(word.charAt(i));
