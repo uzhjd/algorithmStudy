1a66b8eb49a4a88881e3f9880112a383a71be3c4
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-12-23 01:55:57 +0900

    [level 2] Title: H-Index, Time: 0.50 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -10,7 +10,7 @@ class Solution {
             int smaller = Math.min(citations[i], citations.length-i);
             
             if(smaller >= answer)
-                answer = Math.max(answer, smaller);
+                answer = smaller;
             else
                 break;
         }
