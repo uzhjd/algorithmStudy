3b6fdacd03d91349a34cb2c1ab7d55d7e8fd3f72
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-02-23 16:28:10 +0900

    [level 2] Title: 숫자 블록, Time: 108.32 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -5,39 +5,32 @@ import java.util.List;
 class Solution {
 
     public static int[] solution(long begin, long end) {
-        int first = (int)begin;
+        int first = (int) begin;
         int last = (int) end;
         int[] answer = new int[last - first + 1];
-
-        for (int i = first,idx = 0; i <= last; i++) {
-            answer[idx++] = getMaxDivisorExceptMe(i);
+        
+        for(int i = first, idx = 0; i <= last; i++) {
+            answer[idx++] = getBlock(i);
         }
 
         return answer;
     }
 
 
-    private static int getMaxDivisorExceptMe(int x) {
-        if (x == 1) {
-            return 0;
-        }
-
+    private static int getBlock(int n) {
         List<Integer> list = new ArrayList<>();
-
-        for (int i = 2; i <= Math.floor(Math.sqrt(x)); i++) {
-            if (x % i == 0) {
+        if(n == 1) return 0;
+        
+        for(int i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+            if(n % i == 0) {
                 list.add(i);
-                if (x / i <= 10_000_000) {
-                    return x/i;
-                }
+                if(n/i <= 10_000_000)
+                    return n/i;
             }
-
         }
         
-        if (!list.isEmpty()) {
-            return list.get(list.size() - 1);
-        }
-
+        if(!list.isEmpty()) return list.get(list.size() - 1);
+        
         return 1;
     }
 
