1bcb7b4589a969a3cc235c43655af4b65489dc20
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-02-24 20:21:48 +0900

    Update 1. 큰 수 출력하기.java

--- Inflearn/배열/1. 큰 수 출력하기.java
+++ Inflearn/배열/1. 큰 수 출력하기.java
@@ -1,1 +1,24 @@
-
+```java
+  import java.util.*;
+  
+public class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    int n = Integer.parseInt(sc.nextLine());
+    int[] arr = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    
+    System.out.print(arr[0] + " ");
+    for(int i = 1; i < n; i++) {
+      if(arr[i] > arr[i-1])
+        System.out.print(arr[i] + " ");
+    }
+    
+    return ;
+  }
+}
+```
