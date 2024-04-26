d818f498c94e8de0ca00ea74c576ae83c9271792
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-04-26 18:48:42 +0900

    add: unit 7

--- Inflearn/DFS&BFS/합이 같은 부분 집합.java
+++ Inflearn/DFS&BFS/합이 같은 부분 집합.java
@@ -1,9 +1,54 @@
+//import java.util.*;
+//
+//public class Main {
+//
+//    static int target = 0;
+//    static int N;
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        N = sc.nextInt();
+//        int[] arr = new int[N];
+//
+//        for(int i = 0; i < N; i++) {
+//            int n = sc.nextInt();
+//            arr[i] = n;
+//
+//            target += n;
+//        }
+//
+//        if(target % 2 != 0) {
+//            System.out.println("NO");
+//            return;
+//        }
+//        target /= 2;
+//        if(DFS(arr, 0, 0) == 1) System.out.println("YES");
+//        else System.out.println("NO");
+//
+//        return;
+//    }
+//
+//    static public int DFS(int[] arr, int sum, int L) {
+//        if(sum == target)
+//            return 1;
+//        if(L >= N) return 0;
+//
+//        if(DFS(arr, sum + arr[L], L+1) == 1) return 1;
+//        if(DFS(arr, sum, L+1) == 1) return 1;
+//
+//        return 0;
+//    }
+//}
+
+// ver.2.0
 import java.util.*;
 
 public class Main {
 
     static int target = 0;
     static int N;
+    static boolean flag = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -23,20 +68,23 @@ public class Main {
             return;
         }
         target /= 2;
-        if(DFS(arr, 0, 0) == 1) System.out.println("YES");
+        DFS(arr, 0, 0);
+        if(flag) System.out.println("YES");
         else System.out.println("NO");
 
         return;
     }
 
-    static public int DFS(int[] arr, int sum, int L) {
-        if(sum == target)
-            return 1;
-        if(L >= N) return 0;
+    static public void DFS(int[] arr, int sum, int L) {
+        if(sum == target) {
+            flag = true;
+            return;
+        }
+        if(L >= N || flag) return;
 
-        if(DFS(arr, sum + arr[L], L+1) == 1) return 1;
-        if(DFS(arr, sum, L+1) == 1) return 1;
+        DFS(arr, sum + arr[L], L+1);
+        DFS(arr, sum, L+1);
 
-        return 0;
+        return;
     }
 }
