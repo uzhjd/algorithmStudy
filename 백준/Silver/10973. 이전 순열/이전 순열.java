bf181070086b69b4488231556669da9eda366f8c
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-06-16 16:21:54 +0900

    [Silver III] Title: 이전 순열, Time: 464 ms, Memory: 30648 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -1,53 +1,60 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		int[] arr = new int[N];
-		for(int i = 0; i < N; i++)
-			arr[i] = sc.nextInt();
-	
-		
-		if(prePermutaion(arr)) {
-			for(int i : arr)
-				System.out.print(i + " ");
-		} else {
-			System.out.println(-1);
-		}
-	}
-	
-	public static boolean prePermutaion(int[] arr) {
-		int i = arr.length-1;
-		
-		while(i > 0 && arr[i] > arr[i-1]) {
-			i--;
-		}
 
-		if(i == 0)
-			return false;
-		
-		int j = arr.length-1;
-		while(arr[i-1] < arr[j]) {
-			j--;
-		}
-		swap(arr, i-1, j);
-		
-		j = arr.length-1;
-		while(i < j) {
-			swap(arr, i, j);
-			i++;
-			j--;
-		}
-		
-		return true;
-	}
+    static int N;
+    static int[] cur;
 
-	public static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        N = sc.nextInt();
+        cur = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            cur[i] = sc.nextInt();
+        }
+
+        if(perviousPermutation()) {
+            for(int i = 0; i < N; i++) {
+                System.out.print(cur[i] + " ");
+            }
+        } else System.out.println("-1");
+    }
+
+    public static boolean perviousPermutation() {
+        int change = -1;
+        int point = -1;
+
+        for(int i = N - 1; i > 0; i--) {
+            if(cur[i - 1] > cur[i]) {
+                point = i;
+                break;
+            }
+        }
+        if(point == -1) return false;
+
+        for(int i = N - 1; i > 0; i--) {
+            if(cur[point - 1] > cur[i]) {
+                change = i;
+                break;
+            }
+        }
+
+        swap(point - 1, change);
+
+        change = N - 1;
+        while(change > point) {
+            swap(change, point);
+            point++;
+            change--;
+        }
+        return true;
+    }
+
+    public static void swap(int a, int b) {
+        int tmp = cur[a];
+        cur[a] = cur[b];
+        cur[b] = tmp;
+    }
 }
