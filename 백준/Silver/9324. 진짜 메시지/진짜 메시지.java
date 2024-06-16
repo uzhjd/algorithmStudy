3c80cd2c3268eaa53e3d67f51255419f632215e9
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-06-16 17:20:33 +0900

    fix: easy.ver

--- 백준/Silver/9324. 진짜 메시지/진짜 메시지.java
+++ 백준/Silver/9324. 진짜 메시지/진짜 메시지.java
@@ -3,41 +3,35 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int t = scan.nextInt();
-		for(int tc=0; tc<t; tc++) {
-			String str = scan.next();
-			int[] alpa = new int[26];
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for(int i = 0; i < T; i++) {
+			int[] alpha = new int[26];
 			boolean flag = true;
-			String result = "";
-            
-			for(int i=0; i<str.length(); i++) {
-				alpa[str.charAt(i) - 'A'] ++;
-				
-				if(alpa[str.charAt(i) - 'A'] == 3) {
-					
-					if(i == str.length()-1) {
+			String str = sc.next();
+
+			for(int j = 0; j < str.length(); j++) {
+				alpha[str.charAt(j) - 'A']++;
+
+				if(alpha[str.charAt(j) - 'A'] == 3) {
+					if(j == str.length() - 1) {
 						flag = false;
 						break;
 					}
-					
-					if(str.charAt(i) != str.charAt(i+1)) {
+					if(str.charAt(j) != str.charAt(j + 1)) {
 						flag = false;
 						break;
 					}
-					
-					i ++;
-					
-					alpa[str.charAt(i) - 'A'] = 0;
+
+					j ++;
+					alpha[str.charAt(j) - 'A'] = 0;
 				}
 			}
-			result = (flag) ? "OK" : "FAKE";
-			System.out.println(result);
-			
-		}
-		
-		scan.close();
-	}
 
+			String answer = flag ? "OK" : "FAKE";
+			System.out.println(answer);
+		}
+	}
 }
