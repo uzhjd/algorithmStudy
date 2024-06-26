3b31b2a267b55ee656984b730fecbcdba50eced8
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-06-26 20:54:20 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -5,22 +5,17 @@ class Solution {
         int answer = 0;
         int moves = name.length() - 1;
         
-        
         for(int i = 0; i < name.length(); i++) {
-            // 상하
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            int idx = i + 1;
             
-            
-            // 좌우
-            int index = i + 1; // 구해야 할 것
-            while(index < name.length() && name.charAt(index) == 'A') {
-                index++;
+            while(idx < name.length() && name.charAt(idx) == 'A') {
+                idx++;
             }
+            moves = Math.min(moves, i * 2 + name.length() - idx);
+            moves = Math.min((name.length() - idx) * 2 + i, moves);
             
-            // 지금까지의 이동 중 가장 짧은 거 vs i지점에서 꺽어서 뒤로 구하는 것
-            moves = Math.min(moves, i * 2 + name.length() - index);
-            // 꺽고 시작하는게 빠를 수도 있는 경우 체크 e.g.) BBBBBBAAAAAAAB
-            moves = Math.min(moves, (name.length()-index)*2 + i);
+            // alpha
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
         }
         
         return answer + moves;
