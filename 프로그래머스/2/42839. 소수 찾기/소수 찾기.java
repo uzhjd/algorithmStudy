513fea68e8cf38bc3d0698aa8d8bc81d1f429eaa
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-06-27 02:03:35 +0900

    [level 2] Title: 소수 찾기, Time: 13.55 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,39 +3,42 @@ import java.util.*;
 class Solution {
     
     Set<Integer> set = new HashSet<>();
+    int answer;
+    boolean[] check;
     
-    void makeNum(String combin, String str) {
-        if(!combin.equals(""))
-            set.add(Integer.parseInt(combin));
+    public int solution(String numbers) {
+        answer = 0;
+        check = new boolean[numbers.length()];
         
-        for(int i = 0; i < str.length(); i++) {
-            makeNum(combin + str.charAt(i), str.substring(0, i) + str.substring((i + 1), str.length()));
+        dfs("", numbers);
+        
+        return answer;
+    }
+    
+    public void dfs(String str, String numbers) {
+        if(!str.equals("")) isAnswer(str);
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            dfs(str + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
         }
     }
     
-    public Boolean isPrime(int n) {
+    public void isAnswer(String str) {
+        int n = Integer.parseInt(str);
+        if(isPrime(n) && !set.contains(n)) {
+            answer++; 
+            set.add(n);
+        }
+    }
+    
+    public boolean isPrime(int n) {
         if(n < 2) return false;
         
-        int limit = (int)Math.sqrt(n);
-        
-        for(int i = 2; i <= limit; i++) {
+        int mid = (int)Math.sqrt(n);
+        for(int i = 2; i <= mid; i++) {
             if(n % i == 0) return false;
         }
         
         return true;
-    }
-    
-    public int solution(String numbers) {
-        int answer = 0;
-        
-        makeNum("", numbers);
-        
-        
-        for(int n : set) {
-            if(isPrime(n))
-                answer++;
-        }
-        
-        return answer;
     }
 }
