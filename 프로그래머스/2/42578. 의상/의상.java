4fbdaeb7124475f1800c872103e2b650c36ebf36
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-07-19 17:24:28 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,18 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
+        int answer = 1;
         
-        for(String[] clothe : clothes) {
-            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        for(String[] str : clothes) {
+            map.put(str[1], map.getOrDefault(str[1], 0) + 1);
         }
         
         Iterator<Integer> it = map.values().iterator();
-        int answer = 1;
-        
         while(it.hasNext()) {
             answer *= it.next().intValue() + 1;
         }
         
-        return answer - 1 ;
+        return answer - 1;
     }
 }
