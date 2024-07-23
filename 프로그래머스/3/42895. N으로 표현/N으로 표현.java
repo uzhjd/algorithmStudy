3652c4253fa91113e9b83da8f8746d173605cbf2
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-07-23 12:24:33 +0900

    [level 3] Title: N으로 표현, Time: 33.01 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        List<HashSet<Integer>> list = new ArrayList<>();
-        
-        for(int i = 0; i < 9; i ++)
-            list.add(new HashSet<Integer>());
-        
-        list.get(1).add(N);
         if(N == number) return 1;
         
+        List<Set<Integer>> dp = new ArrayList<>();
+        for(int i = 0; i < 9; i++)
+            dp.add(new HashSet<>());
+        
+        dp.get(1).add(N);
         for(int i = 2; i < 9; i++) {
-            HashSet<Integer> now = list.get(i);
+            StringBuilder sb = new StringBuilder();
+            for(int n = 0; n < i; n++)
+                sb.append(N);
+            dp.get(i).add(Integer.parseInt(sb.toString()));
             
             for(int j = 1; j < i; j++) {
-                HashSet<Integer> a = list.get(j);
-                HashSet<Integer> b = list.get(i - j);
+                int k = i - j;
                 
-                for(int x : a) {
-                    for(int y : b) {
-                        now.add(x + y);
-                        now.add(x - y);
-                        now.add(x * y);
-                        if(x != 0 && y != 0)
-                            now.add(x / y);
+                for(int n1 : dp.get(j)) {
+                    for(int n2 : dp.get(k)) {
+                        dp.get(i).add(n1 + n2);
+                        dp.get(i).add(n1 - n2);
+                        dp.get(i).add(n1 * n2);
+                        if(n2 != 0)
+                            dp.get(i).add(n1 / n2);
                     }
                 }
             }
-
-            now.add(Integer.parseInt(String.valueOf(N).repeat(i)));
-            
-            if(now.contains(number)) return i;
         }
         
+        for(int i = 1; i < 9; i++) {
+            if(dp.get(i).contains(number))
+                return i;
+        }
         
         return -1;
     }
