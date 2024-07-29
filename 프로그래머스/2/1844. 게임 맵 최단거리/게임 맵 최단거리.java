7943ccf6e6e46f2515ee31db467e3d30734e529e
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-07-29 21:30:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.92 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,43 +2,51 @@ import java.util.*;
 
 class Solution {
     
-    public int BFS (int[][] maps) {
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
-        
-        int[][] visited = new int[maps.length][maps[0].length];
-        visited[0][0] = 1;
-        
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
-            int cx = now[0];
-            int cy = now[1];
-            
-            if(cx == maps.length - 1 && cy == maps[0].length - 1) {
-                return visited[cx][cy];
-            }
-            
-            for(int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                
-                if(nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    q.add(new int[]{nx, ny});
-                    visited[nx][ny] = visited[cx][cy] + 1;
-                }
-            }
-        }
-        
-        return -1;
-    }
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
     
     public int solution(int[][] maps) {
         int answer = 0;
         
+        int[][] visited = new int[maps.length][maps[0].length];
+        
+        bfs(maps, visited);
+        answer = visited[maps.length-1][maps[0].length-1];
+        
+        if(answer == 0){
+            answer = -1;
+        }
+        
+        return answer;
+    }
+    
+    public void bfs(int[][] maps, int[][] visited){
+        int x = 0;
+        int y = 0;
+        visited[x][y] = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        
+        while(!queue.isEmpty()){
+            int[] current = queue.remove();
+            int cX = current[0];
+            int cY = current[1];
+            
+            for(int i = 0; i < 4; i++){
+                int nX = cX + dx[i];
+                int nY = cY + dy[i];
+                
+                if(nX < 0 || nX > maps.length-1 || nY < 0 || nY > maps[0].length-1)
+                    continue;
+                
+                if(visited[nX][nY] == 0 && maps[nX][nY] == 1){
+                    visited[nX][nY] = visited[cX][cY] + 1;
+                    queue.add(new int[]{nX, nY});
+                }
+            }
+            
+        }
         
         
-        return BFS(maps);
     }
 }
