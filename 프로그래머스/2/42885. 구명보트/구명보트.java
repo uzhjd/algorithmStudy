0ba8811411a6b87cca7961de2551940889b32cc8
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-08-01 19:31:33 +0900

    [level 2] Title: 구명보트, Time: 10.87 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int min = 0;
+        int L = 0;
+        int R = people.length - 1;
         
         Arrays.sort(people);
         
-        for(int max = people.length - 1; min <= max; max--) {
-            if(people[max] + people[min] <= limit) {
-                min++;
-            }
+        while(L <= R) {
+            if(people[R] + people[L] <= limit) L++;
+            
+            R--;
             answer++;
         }
+        
         return answer;
     }
 }
