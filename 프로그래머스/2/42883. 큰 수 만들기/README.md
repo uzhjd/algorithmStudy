600187bf37d154a87e088f94cc178f6dc0ee70a6
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-08-13 00:40:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 68.88 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
 
 ### 성능 요약
 
-메모리: 93.8 MB, 시간: 38.23 ms
+메모리: 98.5 MB, 시간: 68.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 00:34:09
+2024년 08월 13일 00:40:18
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,14 +4,18 @@ class Solution {
     
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
-        int len = number.length() - k;  // 최종 문자열의 길이
-        for (int i = 0; i < number.length(); i++) {
-            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < number.charAt(i)) {
+        int len = number.length() - k;
+        
+        for(int i = 0; i < number.length(); i++) {
+            while(k > 0 && sb.length() > 0 && number.charAt(i) > sb.charAt(sb.length() - 1)) {
                 sb.deleteCharAt(sb.length() - 1);
                 k--;
             }
+            
             sb.append(number.charAt(i));
         }
-        return sb.substring(0, len).toString();  // len 만큼 잘라서 반환
+        
+        return sb.substring(0, len).toString();
+        // return sb.toString();
     }
 }
