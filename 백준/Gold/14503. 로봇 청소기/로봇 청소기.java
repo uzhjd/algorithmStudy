e63c71a0ab1f7e24034e06ecef1947c566eea1c5
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-08-14 18:29:46 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+
+class Main {
+
+    static int N, M, answer;
+    static int[][] map;
+    static int[] dx = new int[]{0, 1, 0, -1};
+    static int[] dy = new int[]{-1, 0, 1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());;
+        int dir = Integer.parseInt(st.nextToken());
+
+        answer = 1;
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());;
+            }
+        }
+
+        DFS(x, y, dir);
+
+        System.out.println(answer);
+
+        return;
+    }
+
+    public static void DFS(int x, int y, int dir) {
+        map[y][x] = -1;
+
+        for(int i = 0; i < 4; i ++) {
+//            dir--;
+//            if (dir == -1) dir = 3;
+
+dir = (dir + 3) % 4;
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx > -1 && nx < M && ny > -1 && ny < N && map[ny][nx] == 0) {
+                answer++;
+                DFS(nx, ny, dir);
+
+                return;
+            }
+        }
+
+//        dir++;
+//        if (dir == 4) dir = 0;
+        int back = (dir + 2) % 4;
+        int bx = x + dx[back];
+        int by = y + dy[back];
+
+        if(by>=0 && by<N && bx>=0 && bx<M && map[by][bx] != 1) DFS(bx,by,dir);
+    }
+}
