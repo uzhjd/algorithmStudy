424e489cbdae1c9b64defb8b671f6aa70890e6fa
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2024-11-01 17:45:45 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=java) 
 
 ### 성능 요약
 
-메모리: 52.2 MB, 시간: 0.02 ms
+메모리: 52.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 18:58:16
+2024년 11월 01일 17:45:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = n;
-        int nCnt = Integer.bitCount(n);
+        int cnt = Integer.bitCount(n);
         
         while(true) {
-            if(Integer.bitCount(++answer) == nCnt) return answer;
+            if(Integer.bitCount(++answer) == cnt) break;
         }
         
-        
+        return answer;
     }
 }
