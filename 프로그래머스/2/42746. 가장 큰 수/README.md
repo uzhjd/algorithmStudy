31aed9c8cf3e09398681d8ed108ad71831ccdcc1
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-02-03 16:59:14 +0900

    [level 2] Title: 가장 큰 수, Time: 247.42 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 1.73 ms
+메모리: 135 MB, 시간: 247.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 10:57:17
+2025년 02월 03일 16:59:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] num = new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
+
+        String[] strNumbers = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+
+        for (String num : strNumbers) {
+            sb.append(num);
+        }
+
+        if (sb.toString().charAt(0) == '0') {
+            return "0";
+        }
         
-        for(int i = 0; i < numbers.length; i++)
-            num[i] = String.valueOf(numbers[i]);
-        
-        Arrays.sort(num, new Comparator<>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
-            }
-        });
-        
-        for(int i = 0; i < numbers.length; i++)
-            answer += num[i];
-        
-        if(answer.charAt(0) == '0') answer = "0";
-        
-        return answer;
+        return sb.toString();
     }
 }
