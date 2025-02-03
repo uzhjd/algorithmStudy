65e42e1a95fa88995fe5f32b58fe56ccb59a27ed
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-02-03 19:05:28 +0900

    [level 1] Title: 폰켓몬, Time: 4.51 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-		int n = nums.length/2;
-
-		HashSet<Integer> hashSet = new HashSet<Integer>();
-		for(int i=0; i<nums.length; i++) {
-			hashSet.add(nums[i]);
-		}
-		if(n < hashSet.size()) answer = n;
-		else answer = hashSet.size();
+        int answer = 1;
+        int n = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        
+        set.add(nums[0]);
+        for(int i = 1; i < nums.length; i++) {
+            if(answer >= n) break;
+            int num = nums[i];
+            if(set.contains(num)) continue;
+            
+            set.add(num);
+            answer++;
+        }
+		
 		return answer;
     }
 }
