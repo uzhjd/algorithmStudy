86afecd20c34ced2f2d3ce03b6993bb01a58cc4e
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-02-05 14:37:25 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,36 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> tmp = new LinkedList<>();
+        int len = progresses.length;
+        int[] days = new int[len];
+        Queue<Integer> que = new LinkedList<>();
         
-        for(int i = 0; i < progresses.length; i++) {
-            int remain = 100 - progresses[i];
-            
-            if(remain % speeds[i] != 0) {
-                remain = remain / speeds[i] + 1;
-            } else remain /= speeds[i];
-            
-            q.add(remain);
+        for(int i = 0; i < len; i++) {
+            days[i] = (100 - progresses[i]) % speeds[i] == 0 ? (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
         }
         
-        int now = q.poll();
+        int max = days[0];
         int cnt = 1;
-        while(!q.isEmpty()) {
-            int n = q.poll();
-            
-            if(n > now) {
-                tmp.add(cnt);
-                now = n;
+        for(int i = 1; i < len; i++) {
+            int n = days[i];
+            if(max < n) {
+                que.add(cnt);
                 cnt = 1;
+                max = n;
             }
-            else cnt++;
-        }       
-        tmp.add(cnt);
-        int[] answer = new int[tmp.size()];
-        int len = tmp.size();
-        for(int i = 0; i < len; i++) {
-            answer[i] = tmp.poll();
+            else {
+                cnt++;
+            }
+        }
+        que.add(cnt);
+        
+        int size = que.size();
+        int[] answer = new int[size];
+        for(int i = 0; i < size; i++) {
+            answer[i] = que.poll();
         }
         
         return answer;
