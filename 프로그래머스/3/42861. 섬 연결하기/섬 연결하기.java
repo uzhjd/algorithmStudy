d3a665eeec12b4881874cb8a9b65026033118662
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-02-13 03:05:31 +0900

    [level 3] Title: 섬 연결하기, Time: 0.58 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,55 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static int[] parent;
-
+    
+    int[] check;
+    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        parent = new int[n];
+        check = new int[n];
         
-        // Union-Find 구조 초기화
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        for(int i = 0; i < n; i++) {
+            check[i] = i;
         }
         
-        // 비용을 기준으로 간선 정렬
         Arrays.sort(costs, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[2], o2[2]);
+                return o1[2] - o2[2];
             }
         });
         
-        for (int i = 0; i < costs.length; i++) {
-            int a = costs[i][0];
-            int b = costs[i][1];
-            int cost = costs[i][2];
+        for(int[] cost : costs) {
+            int from = cost[0];
+            int end = cost[1];
+            int dis = cost[2];
             
-            // 두 노드가 같은 집합에 있지 않은 경우에만 간선 추가
-            if (find(a) != find(b)) {
-                union(a, b);
-                answer += cost;
+            int fa = find(from);
+            int fb = find(end);
+            
+            if(fa != fb) {
+                union(from, end);
+                answer += dis;
             }
         }
         
         return answer;
     }
-
-    // Find 연산
-    private int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
+    
+    public void union(int a, int b) {
+        int fa = find(a);
+        check[fa] = b;
     }
-
-    // Union 연산
-    private void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        
-        if (rootX != rootY) {
-            parent[rootY] = rootX;
-        }
+    
+    public int find(int v) {
+        if(v == check[v]) return v;
+        return check[v] = (find(check[v]));
     }
 }
