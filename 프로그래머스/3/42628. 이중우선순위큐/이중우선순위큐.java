422a51d24dfb49a2fba7ad554df9fefa9450cb0f
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-02-12 19:57:18 +0900

    [level 3] Title: 이중우선순위큐, Time: 343.98 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,39 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> dq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(String s : operations) {
-            String[] tmp = s.split(" ");
-            String a = tmp[0];
-            String b = tmp[1];
-            
-            if(a.equals("I")) {
-                int n = Integer.parseInt(tmp[1]);
-                pq.add(n);
-                dq.add(n);
-            } else {
-                if(b.equals("1") && !dq.isEmpty()) {
-                    int t = dq.poll();
-                    pq.remove(t);
-                } else if (b.equals("-1")  && !pq.isEmpty()){
-                    int t = pq.poll();
-                    dq.remove(t);
+        LinkedList<Integer> list = new LinkedList<>(); // 정렬된 리스트 사용
+
+        for (String s : operations) {
+            if (s.charAt(0) == 'I') { // 삽입 연산
+                int n = Integer.parseInt(s.substring(2));
+                list.add(n);
+                Collections.sort(list); // 항상 정렬 유지
+            } else if (!list.isEmpty()) { // 삭제 연산
+                if (s.charAt(2) == '1') { // 최댓값 삭제
+                    list.removeLast();
+                } else { // 최솟값 삭제
+                    list.removeFirst();
                 }
             }
         }
-        
-        int[] answer = new int[2];
-        if(pq.isEmpty() && dq.isEmpty()) {
-            answer[0] = 0;
-            answer[1] = 0;
-        }
-        else {
-            answer[0] = dq.poll();
-            answer[1] = pq.poll();
-        }
-        
-        return answer;
+
+        if (list.isEmpty()) return new int[]{0, 0};
+        return new int[]{list.getLast(), list.getFirst()};
     }
 }
