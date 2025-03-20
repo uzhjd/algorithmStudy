62ade27f6844993c41cdb81608add05f4ca0830b
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-03-20 11:50:26 +0900

    [level 3] Title: N으로 표현, Time: 38.24 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -4,34 +4,35 @@ class Solution {
     public int solution(int N, int number) {
         if(N == number) return 1;
         
-        List<Set<Integer>> dp = new ArrayList<>();
-        for(int i = 0; i < 9; i++)
-            dp.add(new HashSet<>());
+        List<Set<Integer>> list = new LinkedList<>();
         
-        dp.get(1).add(N);
+        for(int i = 0; i < 9; i++) {
+            list.add(new HashSet<>());
+        }
+        
+        list.get(1).add(N);
         for(int i = 2; i < 9; i++) {
             StringBuilder sb = new StringBuilder();
-            for(int n = 0; n < i; n++)
+            for(int j = 0; j < i; j++)
                 sb.append(N);
-            dp.get(i).add(Integer.parseInt(sb.toString()));
+            list.get(i).add(Integer.parseInt(sb.toString()));
             
             for(int j = 1; j < i; j++) {
                 int k = i - j;
-                
-                for(int n1 : dp.get(j)) {
-                    for(int n2 : dp.get(k)) {
-                        dp.get(i).add(n1 + n2);
-                        dp.get(i).add(n1 - n2);
-                        dp.get(i).add(n1 * n2);
+                for(int n1 : list.get(j)) {
+                    for(int n2 : list.get(k)) {
+                        list.get(i).add(n1 + n2);
+                        list.get(i).add(n1 - n2);
+                        list.get(i).add(n1 * n2);
                         if(n2 != 0)
-                            dp.get(i).add(n1 / n2);
+                            list.get(i).add(n1 / n2);
                     }
                 }
             }
         }
         
-        for(int i = 1; i < 9; i++) {
-            if(dp.get(i).contains(number))
+         for(int i = 1; i < 9; i++) {
+            if(list.get(i).contains(number))
                 return i;
         }
         
