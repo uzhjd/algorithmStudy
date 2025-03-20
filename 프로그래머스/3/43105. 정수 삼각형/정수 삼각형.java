a9299999fc81a4d696496e04ec04422b2861ef39
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-03-20 12:12:39 +0900

    [level 3] Title: 정수 삼각형, Time: 14.27 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,24 +1,32 @@
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        
         int h = triangle.length;
-        int[][] tmp = new int[h][h];
-        tmp[0][0] = triangle[0][0];
+        int w = triangle[h-1].length;
+        int[][] board = new int[h][w];
         
+        board[0][0] = triangle[0][0];
         for(int i = 0; i < h-1; i++) {
-            int w = triangle[i].length;
-            
-            for(int j = 0; j < w; j++) {
-                tmp[i+1][j] = Math.max(triangle[i+1][j] + tmp[i][j], tmp[i+1][j]);
-                tmp[i+1][j+1] = triangle[i+1][j+1] + tmp[i][j];
+            for(int j = 0; j < i + 1; j++) {
+                int t1 = board[i][j] + triangle[i + 1][j];
+                if(board[i + 1][j] != 0) t1 = Math.max(board[i + 1][j], t1);
+                board[i + 1][j] = t1;
+                
+                int t2 = board[i][j] + triangle[i + 1][j + 1];
+                if(board[i + 1][j+1] != 0) t2 = Math.max(board[i + 1][j + 1], t2);
+                board[i + 1][j + 1] = t2;
             }
         }
         
-        for(int n : tmp[h-1]) {
-            answer = Math.max(n, answer);
-        }
+        // for(int i = 0; i < h; i++) {
+        //     for(int j = 0; j < i+1; j ++) {
+        //         System.out.print(board[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        int max = Integer.MIN_VALUE;
+        for(int n : board[h-1])
+            max = Math.max(max, n);
         
-        return answer;
+        return max;
     }
 }
