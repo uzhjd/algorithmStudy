827af595e88596f8e92fac7602f1368418e6f403
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-07-01 00:04:58 +0900

    [level 2] Title: 기능개발, Time: 0.29 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,34 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int len = progresses.length;
-        int[] days = new int[len];
+        int[] task = new int[len];
+        Stack<Integer> stack = new Stack<>();
         Queue<Integer> que = new LinkedList<>();
         
         for(int i = 0; i < len; i++) {
-            days[i] = (100 - progresses[i]) % speeds[i] == 0 ? (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
+            task[i] = (100 - progresses[i]) % speeds[i] == 0 ? (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
         }
         
-        int max = days[0];
-        int cnt = 1;
-        for(int i = 1; i < len; i++) {
-            int n = days[i];
-            if(max < n) {
+        stack.push(task[0]);
+        int cnt = 0;
+        for(int n : task) {
+            if(stack.peek() >= n) cnt++;
+            else {
+                stack.push(n);
                 que.add(cnt);
                 cnt = 1;
-                max = n;
-            }
-            else {
-                cnt++;
             }
         }
+        
         que.add(cnt);
-        
-        int size = que.size();
-        int[] answer = new int[size];
-        for(int i = 0; i < size; i++) {
-            answer[i] = que.poll();
-        }
-        
+        int[] answer = new int[que.size()];
+        int idx = 0;
+        while(!que.isEmpty()) answer[idx++] = que.poll();
         return answer;
     }
 }
