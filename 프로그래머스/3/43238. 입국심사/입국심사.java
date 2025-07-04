4cc4e09dfe11d2c1a7e37483dfed7a9335d52a61
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-07-04 17:04:21 +0900

    [level 3] Title: 입국심사, Time: 127.26 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,28 @@
 import java.util.Arrays;
+
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
         Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
+        int len = times.length;
+        long min = times[0];
+        long max = times[len - 1] * (long)n;
+        long answer = -1;
         
-        while(left <= right) {
-            long mid = (left + right) / 2;
-            long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
-                left = mid + 1;
-            else {
-                right = mid - 1;
-                answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+        while(min <= max) {
+            long mid = (min + max) / 2;
+            long tmp = 0;
+            
+            for(int num : times) {
+                tmp += (mid / num);
             }
-        }  
+            
+            if(n > tmp) min = mid + 1;
+            else {
+                max = mid -1;
+                answer = mid;
+            }
+        }
+        
         return answer;
     }
 }
