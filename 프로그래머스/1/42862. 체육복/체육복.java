c7a7842d32b8a7b6975fe21c2ddd5326efba062c
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2025-07-08 02:18:37 +0900

    [level 1] Title: 체육복, Time: 0.58 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int cnt = 0;
-        
+        int answer = n - lost.length;
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
         for(int i = 0; i < lost.length; i++) {
             for(int j = 0; j < reserve.length; j++) {
                 if(lost[i] == reserve[j]) {
-                    cnt++;
+                    answer++;
                     lost[i] = -1;
                     reserve[j] = -1;
                     break;
@@ -21,17 +20,16 @@ class Solution {
         
         for(int i = 0; i < lost.length; i++) {
             for(int j = 0; j < reserve.length; j++) {
-                int l = lost[i];
-                int r = reserve[j];
-                
-                if(l == r - 1 || l == r + 1) {
-                    cnt++;
+                if(lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1) {
+                    answer++;
+                    lost[i] = -1;
                     reserve[j] = -1;
                     break;
                 }
+                if(lost[i] < reserve[j]) break;
             }
         }
         
-        return n - lost.length + cnt;
+        return answer;
     }
 }
